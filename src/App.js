7219eec17499a0ce50368c4e1c83af14bcf22cac
Ryan Erkal <ryanerkal@yahoo.ie>
2023-08-15 09:37:18 +0100

    fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React from "react";
 import { useState } from "react";
+import Question from "./Question";
 import "./App.css";
 
-function App() {
+export default function App() {
 	const [start, setStart] = useState(false);
+	const [data, setData] = useState();
+	const [dataStatus, setDataStatus] = useState("not-loaded");
+	const [curChar, setCurChar] = useState();
+
+	async function fetchData() {
+		setDataStatus("loading");
+		const response = await fetch("https://swapi.dev/api/people/");
+		const data = await response.json();
+		setDataStatus("loaded");
+		setData(data.results);
+		//console.log(data.results[1]);
+	}
+
+	React.useEffect(() => {
+		fetchData();
+	}, []);
+
+	function newChar() {
+		const random = Math.floor(Math.random() * data.length);
+		//setCurChar(data[random]);
+		console.log(data[random]);
+		setCurChar(data[random]);
+	}
 
 	return (
-		<div className="App">
-			{!start && <h1>Quizzical</h1>}
-			{!start && <button onClick={() => setStart(true)}>Start</button>}
-		</div>
+		<>
+			<h1>Quizzical</h1>
+			<Question question="Test Q" />
+			{dataStatus === "loaded" && (
+				<div>
+					<button onClick={newChar}>New Character</button>
+				</div>
+			)}
+			<div>{dataStatus} data</div>
+			{curChar && <div>{curChar.name}</div>}
+		</>
 	);
 }
-
-export default App;
